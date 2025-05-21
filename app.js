fe84c9c6e262c30e93cf7854c141c18636324b4f
ProGamingVN <tienhm118@gmail.com>
2025-05-21 14:21:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+let isRunning = false;
+  let isPaused = false;
+  let speedMultiplier = 1;
+
+  function togglePause() 
+  {
+    isPaused = !isPaused;
+    document.querySelector('button[onclick="togglePause()"]').innerText = isPaused ? 'Tiếp tục' : 'Tạm dừng';
+  }
 
